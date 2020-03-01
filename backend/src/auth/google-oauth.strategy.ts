76a5ac94d167fa6b9265f59e476ba049adc6a0fc
Wang Guan <momocraft@gmail.com>
2020-03-02 01:58:50 +0900

    wip

--- backend/src/auth/google-oauth.strategy.ts
+++ backend/src/auth/google-oauth.strategy.ts
@@ -1,8 +1,11 @@
+import express from 'express';
 import { AuthGuard, PassportStrategy } from '@nestjs/passport';
 import { Profile, Strategy } from 'passport-google-oauth20';
 import { UserEntity } from '../entity/user.entity';
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { getDebugLogger } from '../util/get-debug-logger';
+
+const logger = getDebugLogger(__filename);
 
 @Injectable()
 export class GoogleOauthStrategy extends PassportStrategy(Strategy, 'google') {
@@ -10,15 +13,26 @@ export class GoogleOauthStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: 'FILL ME',
       clientSecret: 'FILL_ME',
-      scope: ['profile', 'email'],
-      callbackURL: 'FILL_ME', // have to match the one retrieved code?
+      callbackURL: 'FILL_ME', // TODO: this should be param in code
+      passReqToCallback: true,
     });
   }
 
   async validate(accessToken: string, refreshToken: string, profile: Profile): Promise<UserEntity> {
-    getDebugLogger(__filename)('%s / %s / %O', accessToken, refreshToken, profile);
+    logger('%s / %s / %O', accessToken, refreshToken, profile);
 
     throw new InternalServerErrorException('something is wrong');
+  }
+
+  authenticate(req: express.Request, options?: any): void {
+    logger('req: %O', req);
+    logger('options: %O', options);
+    super.authenticate(req, options);
+  }
+
+  authorizationParams(options: any): object {
+    logger('params: %O');
+    return super.authorizationParams(options);
   }
 }
 
