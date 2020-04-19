3a20eec7246e97c99da7082ae78c59f20c466d75
Wang Guan <momocraft@gmail.com>
2020-04-20 08:34:01 +0900

    remove unintented index in migration

--- backend/src/db/migrations/1587318828205-AddCreateUpdateTimestamp.ts
+++ backend/src/db/migrations/1587318828205-AddCreateUpdateTimestamp.ts
@@ -4,7 +4,6 @@ export class AddCreateUpdateTimestamp1587318828205 implements MigrationInterface
   name = 'AddCreateUpdateTimestamp1587318828205';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP INDEX "IDX_2eef64213ff1f87ea81e128062f747bc"`, undefined);
     await queryRunner.query(`ALTER TABLE "o_auth_account" ADD "createdAt" TIMESTAMP NOT NULL DEFAULT now()`, undefined);
     await queryRunner.query(`ALTER TABLE "o_auth_account" ADD "updatedAt" TIMESTAMP NOT NULL DEFAULT now()`, undefined);
     await queryRunner.query(`ALTER TABLE "user_account" ADD "createdAt" TIMESTAMP NOT NULL DEFAULT now()`, undefined);
@@ -16,9 +15,5 @@ export class AddCreateUpdateTimestamp1587318828205 implements MigrationInterface
     await queryRunner.query(`ALTER TABLE "user_account" DROP COLUMN "createdAt"`, undefined);
     await queryRunner.query(`ALTER TABLE "o_auth_account" DROP COLUMN "updatedAt"`, undefined);
     await queryRunner.query(`ALTER TABLE "o_auth_account" DROP COLUMN "createdAt"`, undefined);
-    await queryRunner.query(
-      `CREATE INDEX "IDX_2eef64213ff1f87ea81e128062f747bc" ON "user_account" ("shortId") `,
-      undefined,
-    );
   }
 }
