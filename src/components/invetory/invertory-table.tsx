15935761df708fbe451b584c1e266edd4868dd00
Wang Guan <momocraft@gmail.com>
2020-04-26 04:59:41 +0900

    wip

--- src/components/invetory/invertory-table.tsx
+++ src/components/invetory/invertory-table.tsx
@@ -2,6 +2,8 @@ import React, { useMemo } from 'react';
 import { dynamicItemsV1 } from '../../json/dynamic-items-v1';
 import { usePromised } from '@jokester/ts-commonutil/react/hook/use-promised';
 import { ItemsV1Json } from '../../json/json';
+import { ItemColumnType } from '../../model/item-id-def';
+import { FontAwesomeIcon } from '../icon/fontawesome-icon';
 
 export const InventoryDb: React.FC = () => {
   const itemsDbP = useMemo(dynamicItemsV1, []);
@@ -24,27 +26,50 @@ const InventoryTableSheet: React.FC<{ sheet: ItemsV1Json.Sheet }> = ({ sheet }) 
   const columns = sheet.headers;
   if (!columns) return null;
 
-  const th = <thead>{}</thead>;
+  const thead = (
+    <thead key={-1}>
+      <tr>
+        <td>名字</td>
+        <td>图片</td>
+        <td>变体</td>
+        <td>meta</td>
+      </tr>
+    </thead>
+  );
+
+  const tbodyRows = useMemo(
+    () =>
+      sheet.items.map((item, itemNo) => (
+        <tr key={itemNo}>
+          <td>
+            {/* name */ [
+              item[ItemColumnType.nameZhS],
+              <br />,
+              item[ItemColumnType.nameZhT],
+              <br />,
+              item[ItemColumnType.nameJa],
+              <br />,
+              item[ItemColumnType.nameEn],
+            ]}
+          </td>
+          <td>
+            {/* pic */}
+            <FontAwesomeIcon large iconName="fa-images" />
+          </td>
+          <td>TODO</td>
+          <td>TODO</td>
+        </tr>
+      )),
+    [sheet.items],
+  );
+
   return (
     <div>
       <h2>{sheet.sheetName}</h2>
+      <table className="w-full">
+        {thead}
+        <tbody>{tbodyRows}</tbody>
+      </table>
     </div>
   );
 };
-
-function columnsToShow(columnName?: string) {
-  switch(columnName) {
-    case '';
-
-      简体中文: string;
-      繁体中文: string;
-      日文: string;
-      英文: string;
-      类型?: string;
-      序号?: string;
-      颜色?: string;
-
-  }
-
-
-}
--- src/model/item-id-def.ts
+++ src/model/item-id-def.ts
@@ -1,3 +1,33 @@
 export function itemIdDef(sheetIndex: number, row: number) {
   return `mo-${sheetIndex}-${row}`;
 }
+
+export const enum ItemColumnType {
+  // name
+  nameZhS = '简体中文',
+  nameZhT = '繁体中文',
+  nameJa = '日文',
+  nameEn = '英文',
+  // variants?
+  customizationA = '改造类型A',
+  customizationB = '改造类型B',
+  color = '颜色',
+  // meta?
+  type = '类型',
+  orderType = '订购类型',
+  orderType2 = '订购类型2',
+  detailType = '细分类型',
+  size = '尺寸',
+  // '出现场所',
+  // '鱼影尺寸',
+  // '北半球出现季节',
+  // '南半球出现季节',
+  // '出现时间',
+  // '北半球月份',
+  // '南半球月份',
+  // '时间',
+  // '价格',
+  // id?
+  officialItemId = '官方序号',
+  itemId = '序号',
+}
