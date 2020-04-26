361a8d9ab8b2533bab31b4a0bd489d2a2c80ebfa
Wang Guan <momocraft@gmail.com>
2020-04-26 22:11:21 +0900

    add migration: UserAccountSyncIndex

---
+++ backend/src/db/migrations/1587906643180-UserAccountSyncIndex.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class UserAccountSyncIndex1587906643180 implements MigrationInterface {
+  name = 'UserAccountSyncIndex1587906643180'
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP INDEX "IDX_2eef64213ff1f87ea81e128062f747bc"`, undefined);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE INDEX "IDX_2eef64213ff1f87ea81e128062f747bc" ON "user_account" ("shortId") `, undefined);
+  }
+
+}
