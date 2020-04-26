6d97ed487557e5ee131fff6f70dd78526941f1ad
Wang Guan <momocraft@gmail.com>
2020-04-26 14:54:54 +0900

    better mobile

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -31,6 +31,7 @@ export default class CustomDocument extends Document {
             integrity="sha256-TCBL18Dgf7kFC0vKWhEaFD+87xslflRbMTAaChRCyTc="
             crossOrigin="anonymous"
           />
+          <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1.5,minimum-scale=0.8" />
         </Head>
         <body className="bg-dark">
           <Main />
