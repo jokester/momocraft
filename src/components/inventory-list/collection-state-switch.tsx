45955ced51d32031836385e15941c0cca7d50ada
Wang Guan <momocraft@gmail.com>
2020-05-01 01:17:51 +0900

    wip

--- src/components/inventory-list/collection-state-switch.tsx
+++ src/components/inventory-list/collection-state-switch.tsx
@@ -6,14 +6,30 @@ import { Button, ButtonGroup } from '@blueprintjs/core';
 export const CollectionStateSwitch: React.FC<{ item: ItemsV2Json.Item }> = props => {
   const [status, setStatus] = useState<PossessionState>(randomPossessionState);
   return (
-    <ButtonGroup fill vertical>
-      <Button onClick={() => setStatus(PossessionState.own)} active={status === PossessionState.own} icon="tick-circle">
+    <ButtonGroup vertical>
+      <Button
+        small
+        onClick={() => setStatus(PossessionState.own)}
+        active={status === PossessionState.own}
+        className="text-xl"
+        icon="tick-circle"
+      >
         拥有
       </Button>
-      <Button onClick={() => setStatus(PossessionState.with)} active={status === PossessionState.with} icon="hand">
+      <Button
+        small
+        onClick={() => setStatus(PossessionState.with)}
+        active={status === PossessionState.with}
+        icon="hand"
+      >
         想摸
       </Button>
-      <Button onClick={() => setStatus(PossessionState.none)} active={status === PossessionState.none} icon="remove">
+      <Button
+        small
+        onClick={() => setStatus(PossessionState.none)}
+        active={status === PossessionState.none}
+        icon="remove"
+      >
         取消
       </Button>
     </ButtonGroup>
--- src/components/inventory-list/inventory-card-list.tsx
+++ src/components/inventory-list/inventory-card-list.tsx
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Link from 'next/link';
 import { TypedRoutes } from '../../typed-routes';
 import { ItemsV2Json } from '../../json/json';
 import { createAspectRatioStyle } from '../../style/aspect-ratio';
+import { CollectionStateSwitch } from './collection-state-switch';
+import { ItemUtils } from '../../json/item-utils';
 
-const InventoryCard: React.FunctionComponent<{ item: ItemsV2Json.Item }> = props => (
-  <Link href={TypedRoutes.index}>
-    <div className="inline-block my-2 w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 h-32">
+const InventoryCard: React.FunctionComponent<{ item: ItemsV2Json.Item }> = ({ item }) => {
+  const title = useMemo(() => ItemUtils.extractDisplayName(item), [item]);
+  return (
+    <div className="inline-block my-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/5 h-32">
       <div
-        className="model-cell bg-blue-900 my-1 mx-2 h-full flex-col rounded-lg"
+        className="model-cell bg-blue-100 border border-solid border-blue-300 my-1 mx-2 h-full flex-col rounded-lg p-2"
         style={createAspectRatioStyle(16 / 10)}
       >
-        <div className="bg-gray-100" />
-        <div className="bg-gray-200 " />
+        <h3 className="text-xl">{title}</h3>
+        <div className="flex mt-2 justify-around">
+          <img
+            src="https://dummyimage.com/400x300/cff/000"
+            alt="image"
+            className="max-h-full h-20 object-cover inline-block bg-blue-200 mr-2"
+          />
+          <CollectionStateSwitch item={item} />
+        </div>
       </div>
     </div>
-  </Link>
-);
+  );
+};
 
 export const DummyModelListHeader: React.FunctionComponent<{ title: string }> = ({ title }) => (
   <h3 className="px-2 my-1 font-bold ">{title}</h3>
