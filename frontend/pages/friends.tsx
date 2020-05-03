ee2bb7638c2702cc8b62a1e8e772a77498296358
Wang Guan <momocraft@gmail.com>
2020-05-04 02:37:58 +0900

    wip

---
+++ frontend/pages/friends.tsx
@@ -0,0 +1,6 @@
+import { PageType } from '../src/next-types';
+import { Layout } from '../src/components/layout/layout';
+import React from 'react';
+import { WorkingPage } from '../src/components/dummy/working-page';
+
+export default WorkingPage;
