1a1df38b5c68ca32b2df737934811f0680c796ac
Wang Guan <momocraft@gmail.com>
2020-05-04 03:24:50 +0900

    fix ssr

--- frontend/src/components/dummy/working-page.tsx
+++ frontend/src/components/dummy/working-page.tsx
@@ -1,8 +1,9 @@
 import { CenterH, Layout } from '../layout/layout';
 import React from 'react';
+import { PageType } from '../../next-types';
 
-export const WorkingPage: React.FC = () => (
+export const WorkingPage: PageType = props => (
   <Layout>
-    <CenterH className="mt-24">施工中: {location.pathname}</CenterH>
+    <CenterH className="mt-24">施工中: {props.route.pathname}</CenterH>
   </Layout>
 );
