db2935cc80944b18b197efbc51f8fc704b3a615a
Wang Guan <momocraft@gmail.com>
2020-05-04 17:14:40 +0900

    render item cell in view

--- frontend/src/components/inventory-list/inventory-card-list.tsx
+++ frontend/src/components/inventory-list/inventory-card-list.tsx
@@ -25,20 +25,22 @@ export const InventoryCard: React.FunctionComponent<{
 
   return (
     <div className="inline-block my-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/5 h-40" ref={ref}>
-      <div className="bg-blue-100 border border-solid border-blue-300 my-1 mx-2 h-full flex-col rounded-lg p-2">
-        <div className="flex justify-between align-baseline">
-          <h3 className="text-xl inline-block">{title}</h3>
-          <Link href={TypedRoutes.items.show2(item)}>查看详细</Link>
+      {visible && (
+        <div className="bg-blue-100 border border-solid border-blue-300 my-1 mx-2 h-full flex-col rounded-lg p-2">
+          <div className="flex justify-between align-baseline">
+            <h3 className="text-xl inline-block">{title}</h3>
+            <Link href={TypedRoutes.items.show2(item)}>查看详细</Link>
+          </div>
+          <div className="flex mt-2 justify-around">
+            <img
+              src="https://dummyimage.com/400x300/cff/000"
+              alt="image"
+              className="max-h-full h-20 object-cover inline-block bg-blue-200 mr-2"
+            />
+            <CollectionStateSwitch item={item} collectionMap={collectionMap} />
+          </div>
         </div>
-        <div className="flex mt-2 justify-around">
-          <img
-            src="https://dummyimage.com/400x300/cff/000"
-            alt="image"
-            className="max-h-full h-20 object-cover inline-block bg-blue-200 mr-2"
-          />
-          <CollectionStateSwitch item={item} collectionMap={collectionMap} />
-        </div>
-      </div>
+      )}
     </div>
   );
 };
