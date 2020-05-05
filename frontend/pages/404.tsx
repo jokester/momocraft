4b31f22c22a332c6a5751508e769647f6cc92826
Wang Guan <momocraft@gmail.com>
2020-05-05 23:41:55 +0900

    404: fix router

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -1,12 +1,13 @@
 import { PageType } from '../src/next-types';
 import { Layout } from '../src/components/layout/layout';
 import React, { useEffect } from 'react';
-import { Router } from 'next/router';
+import { Router, useRouter } from 'next/router';
 import { TypedRoutes } from '../src/typed-routes';
 
 const NotFoundPage: PageType = props => {
+  const router = useRouter();
   useEffect(() => {
-    Router.replace(TypedRoutes.index);
+    router.replace(TypedRoutes.index);
   }, []);
   return <Layout>还没做</Layout>;
 };
