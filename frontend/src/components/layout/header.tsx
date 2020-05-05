ba28b470a248d2f9e932c9cc81bae28e74d72865
Wang Guan <momocraft@gmail.com>
2020-05-06 03:38:33 +0900

    add highlight in header

--- frontend/src/components/layout/header.tsx
+++ frontend/src/components/layout/header.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import Link from 'next/link';
 import { TypedRoutes } from '../../typed-routes';
 import { FontAwesomeIcon } from '../icon/fontawesome-icon';
+import { useRouter } from 'next/router';
+import classNames from 'classnames';
 
 const SectionLink: React.FC<{ text: string | React.ReactElement; linkTo: string }> = props => {
+  const here = useRouter().pathname;
+  const buttonClass = classNames('text-sm px-2 py-2 mx-2 my-2 rounded-lg border', {
+    ['bg-blue-300 border-solid border-blue-400']: here.startsWith(props.linkTo),
+    ['bg-blue-200']: !here.startsWith(props.linkTo),
+  });
   return (
     <Link href={props.linkTo}>
-      <button className="text-sm px-2 py-2 bg-blue-200 mx-2 my-2 rounded-lg">{props.text}</button>
+      <button className={buttonClass}>{props.text}</button>
     </Link>
   );
 };
