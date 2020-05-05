fe4e96a6730a6ae77fb1d7ece881959995c44ac4
Wang Guan <momocraft@gmail.com>
2020-05-05 19:49:24 +0900

    revert collection state in InventoryDb

--- frontend/src/components/inventory-list/invertory-table.tsx
+++ frontend/src/components/inventory-list/invertory-table.tsx
@@ -3,6 +3,9 @@ import { useItemsDB } from '../hooks/use-items-db';
 import { createLogger } from '../../util/debug-logger';
 import { InventoryCategoryPicker } from './inventory-category-picker';
 import { InventoryCard, InventoryCartListView } from './inventory-card-list';
+import { useAuthState } from '../hooks/use-auth-state';
+import { useCollectionList } from '../hooks/use-collections-api';
+import { RenderPromiseEither } from '../hoc/render-promise-either';
 
 const logger = createLogger(__filename);
 
@@ -18,15 +21,22 @@ export const InventoryDb: React.FC = () => {
     currentSheetId,
   ]);
 
+  const authed = useAuthState();
+
+  const collectionsP = useCollectionList(authed.user?.userId || null);
+
   if (itemsDb.fulfilled) {
     return (
       <div>
-        <InventoryCategoryPicker sheets={itemsDb.value.sheets} curentSheetId={currentSheetId} setSheetId={setSheetId} />
-        <InventoryCartListView key={currentSheetId}>
-          {items.map((item, i) => (
-            <InventoryCard key={item.itemId} item={item} collectionMap={null} lazyLoad={i > 20} />
-          ))}
-        </InventoryCartListView>
+        <RenderPromiseEither promise={collectionsP}>
+          {collections => (
+            <InventoryCartListView key={currentSheetId}>
+              {items.map((item, i) => (
+                <InventoryCard key={item.itemId} item={item} collectionMap={collections} lazyLoad={i > 20} />
+              ))}
+            </InventoryCartListView>
+          )}
+        </RenderPromiseEither>
       </div>
     );
   }
