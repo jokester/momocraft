56ad43ca715b98fd4c611a1385824a3232dc0601
Wang Guan <momocraft@gmail.com>
2020-05-25 02:01:51 +0900

    run prettier on generated code

--- frontend/scripts/generate-api-client.sh
+++ frontend/scripts/generate-api-client.sh
@@ -10,8 +10,11 @@ rm -rf $GENERATED_ROOT && mkdir -pv $GENERATED_ROOT
 
 curl http://127.0.0.1:3001/api-json > $GENERATED_ROOT/openapi.json
 
+export TS_POST_PROCESS_FILE="yarn prettier --write"
+
 yarn openapi-generator generate      \
     -g typescript-fetch              \
     -c scripts/typescript-fetch.json \
+    --enable-post-process-file       \
     -i $GENERATED_ROOT/openapi.json  \
     -o $GENERATED_ROOT
