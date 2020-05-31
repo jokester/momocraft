3d3c7836743290fbc2e82f2e39be41d39336fc0d
Wang Guan <momocraft@gmail.com>
2020-05-31 21:15:58 +0900

    introduce react-i18next

--- frontend/src/components/frontpage/intro.tsx
+++ frontend/src/components/frontpage/intro.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 export const FrontpageIntro: React.FC = () => {
+  const [t] = useTranslation();
   return (
     <div className="mt-20 px-4 md:px-8 lg:px-16 leading-tight">
-      <h2 className="text-2xl mb-2 font-semibold">Momocraft</h2>
+      <h2 className="text-2xl mb-2 font-semibold">{t('site.siteName')}</h2>
       <p>
         <span className="font-semibold">Momocraft</span> 是一个帮助动森玩家记录物品收藏和交换收藏 (俗称 "摸摸") 的网站.
       </p>
