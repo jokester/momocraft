dedb5528110104d40a71893130d3b871b59796b2
Wang Guan <momocraft@gmail.com>
2020-06-01 02:02:39 +0900

    remove defaultGetServerSideProps() from 404 page

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -3,16 +3,14 @@ import { Layout } from '../src/components/layout/layout';
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { TypedRoutes } from '../src/typed-routes';
-import { defaultGetServerSideProps } from '../src/ssr/default-get-server-side-props';
 
 const NotFoundPage: PageType = (props) => {
   const router = useRouter();
   useEffect(() => {
+    // TODO: set prefered lang from navigator.acceptLang
     router.replace(TypedRoutes.index);
   }, []);
   return <Layout>还没做</Layout>;
 };
 
 export default NotFoundPage;
-
-export const getServerSideProps = defaultGetServerSideProps;
