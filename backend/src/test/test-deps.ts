e3ff3b93c73bc352ecf89001480e6fd193b2cc95
Wang Guan <momocraft@gmail.com>
2020-06-20 23:42:04 +0900

    not working: reset db with migrations

--- backend/src/test/test-deps.ts
+++ backend/src/test/test-deps.ts
@@ -7,6 +7,7 @@ import { GoogleOAuthResponse } from '../user/google-oauth.service';
 import { JwtService } from '@nestjs/jwt';
 import { EmailAuthRequestDto, OAuthRequestDto } from '../model/auth.dto';
 import { DiscordOAuth } from '../user/oauth-client.provider';
+import path from 'path';
 
 export namespace TestDeps {
   export const testConnection = createConnection({
@@ -14,12 +15,14 @@ export namespace TestDeps {
     url: process.env['TEST_DB_URL'] || 'postgresql://pguser:secret@127.0.0.1:54432/momo_test',
     logger: 'debug',
     entities: [UserAccount, OAuthAccount],
+    migrations: [path.join(__dirname, '../db/migrations')],
   });
 
   export async function resetTestDB(): Promise<void> {
     const conn = await testConnection;
     await conn.transaction(async (em) => await em.query(`DROP SCHEMA public CASCADE; CREATE SCHEMA public;`));
-    await conn.synchronize(true);
+    await conn.showMigrations();
+    await conn.runMigrations();
   }
 
   export const mockedEntropy = new EntropyService();
