a71ccf7fcce96164fed97ca78e787453f8ac0cf5
Wang Guan <momocraft@gmail.com>
2020-07-11 01:46:20 +0900

    add screen scripts

---
+++ dev-frontend.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -uex
+
+cd $(dirname "$0")/frontend
+
+SCREEN_SESSION='momocraft-frontend'
+
+screen -dm -S $SCREEN_SESSION
+
+# window 0: vim
+screen -S $SCREEN_SESSION -p 0 -X stuff "vim ."
+
+# window1: yarn dev
+screen -S $SCREEN_SESSION -X screen 1
+screen -S $SCREEN_SESSION -p 1 -X stuff "yarn dev"
+
+# window2: yarn typecheck
+screen -S $SCREEN_SESSION -X screen 2
+screen -S $SCREEN_SESSION -p 2 -X stuff "yarn typecheck --watch"
+
+exec screen -r $SCREEN_SESSION
+
