65ec5fdae03e8fc7348d09ecb5b78984a8f764d2
Wang Guan <momocraft@gmail.com>
2020-07-22 23:28:05 +0900

    fix type of onChangeEvent

--- frontend/src/components/hooks/use-text-input.ts
+++ frontend/src/components/hooks/use-text-input.ts
@@ -1,11 +1,14 @@
-import { ChangeEvent, useCallback, useState } from 'react';
+import { ChangeEvent, EventHandler, FormEventHandler, useCallback, useState } from 'react';
 
 export function useTextInput(initial?: string) {
   const [text, setText] = useState(initial || '');
 
-  const onChangeEvent = useCallback((ev: ChangeEvent<Element & { value: string }>) => {
-    setText(ev.target.value);
-  }, []);
+  const onChangeEvent: FormEventHandler<Element & { value: string }> = useCallback(
+    (ev: ChangeEvent<Element & { value: string }>) => {
+      setText(ev.target.value);
+    },
+    [],
+  );
 
   return [text, onChangeEvent] as const;
 }
