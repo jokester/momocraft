308747ef1d0dd32f35461f9915326c77d0c29ca2
Wang Guan <momocraft@gmail.com>
2020-08-07 02:03:07 +0900

    fix storybook webpack conf

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -2,43 +2,46 @@ module.exports = {
   stories: ['../src/**/*.stories.tsx'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
   webpackFinal: async (config) => {
-    config.module.rules.push(
-      {
-        test: /\.(ts|tsx)$/,
-        use: [
-          {
-            loader: require.resolve('ts-loader'),
-            options: {
-              compilerOptions: {
-                noEmit: false,
-                target: 'es6',
-                module: 'commonjs',
-                jsx: 'react',
+    config.module.rules = config.module.rules
+      .filter((existingRule) => !existingRule.test.test('some.css'))
+      .concat([
+        {
+          test: /\.(ts|tsx)$/,
+          use: [
+            {
+              loader: require.resolve('ts-loader'),
+              options: {
+                compilerOptions: {
+                  noEmit: false,
+                  target: 'es6',
+                  module: 'commonjs',
+                  jsx: 'react',
+                },
               },
             },
-          },
-          // Optional
-          // { loader: require.resolve('react-docgen-typescript-loader'), },
-        ],
-      },
-      {
-        test: /\.s?css$/,
-        use: [
-          {
-            loader: 'postcss-loader',
-            options: {
-              ident: 'postcss',
-              plugins: [require('tailwindcss'), require('autoprefixer')],
+            // Optional
+            // { loader: require.resolve('react-docgen-typescript-loader'), },
+          ],
+        },
+        {
+          test: /\.s?css$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                ident: 'postcss',
+                plugins: [require('tailwindcss'), require('autoprefixer')],
+                sourceMap: false,
+              },
             },
-          },
-          {
-            loader: 'sass-loader',
-            options: {},
-          },
-          // { loader: 'style-loader', options: {}, },
-        ],
-      },
-    );
+            {
+              loader: 'sass-loader',
+              options: {},
+            },
+          ],
+        },
+      ]);
     config.resolve.extensions.push('.ts', '.tsx');
     return config;
   },
