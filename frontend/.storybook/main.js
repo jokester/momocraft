8e36384d3f9c752a42bfecbe63bd26fa9e27626d
Wang Guan <momocraft@gmail.com>
2020-08-07 01:23:15 +0900

    storybook: configure postcss

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -2,24 +2,43 @@ module.exports = {
   stories: ['../src/**/*.stories.tsx'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
   webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve('ts-loader'),
-          options: {
-            compilerOptions: {
-              noEmit: false,
-              target: 'es6',
-              module: 'commonjs',
-              jsx: 'react',
+    config.module.rules.push(
+      {
+        test: /\.(ts|tsx)$/,
+        use: [
+          {
+            loader: require.resolve('ts-loader'),
+            options: {
+              compilerOptions: {
+                noEmit: false,
+                target: 'es6',
+                module: 'commonjs',
+                jsx: 'react',
+              },
             },
           },
-        },
-        // Optional
-        // { loader: require.resolve('react-docgen-typescript-loader'), },
-      ],
-    });
+          // Optional
+          // { loader: require.resolve('react-docgen-typescript-loader'), },
+        ],
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: [require('tailwindcss'), require('autoprefixer')],
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {},
+          },
+          // { loader: 'style-loader', options: {}, },
+        ],
+      },
+    );
     config.resolve.extensions.push('.ts', '.tsx');
     return config;
   },
