190dee0381f7f24652d6a7206753fb56402e6389
Wang Guan <momocraft@gmail.com>
2020-08-14 03:36:28 +0900

    new stories

--- frontend/src/components/new-layout/header.tsx
+++ frontend/src/components/new-layout/header.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Button } from '@chakra-ui/core';
 
-const HeaderButton: React.FC<unknown> = (props) => <Button backgroundColor="ac-pink.900">{props.children}</Button>;
+import transitionTheme from './transition-theme.module.scss';
+import classNames from 'classnames';
 
-export const Header: React.FC = (props) => {
+export { transitionTheme };
+
+const HeaderButton: React.FC<unknown> = (props) => (
+  <Button size="sm" color="ac-pink.900">
+    {props.children}
+  </Button>
+);
+
+export const Header: React.FC<{ themeClass?: string }> = (props) => {
   return (
-    <div className="bg-ac-green-500 h-64 space-x-2 p-2">
+    <div className={classNames('p-4 ', transitionTheme.transitionThemed, props.themeClass)}>
       <HeaderButton>btn1</HeaderButton>
       <HeaderButton>btn2</HeaderButton>
     </div>
--- frontend/src/stories/3-layout.stories.tsx
+++ frontend/src/stories/3-layout.stories.tsx
@@ -1,8 +1,16 @@
-import { Header } from '../components/new-layout/header';
+import { Header, transitionTheme } from '../components/new-layout/header';
 import React from 'react';
+import { PropOf } from '@jokester/ts-commonutil/lib/react/util/prop-of';
+import { Story, Meta } from '@storybook/react/types-6-0';
 
 export default {
   title: 'new layout',
 };
 
-export const HeaderPreview = () => <Header />;
+export const HeaderPreviewGreen = () => <Header themeClass={transitionTheme.variantG} />;
+export const HeaderPreviewPink = () => <Header themeClass={transitionTheme.variantP} />;
+
+const Template: Story<PropOf<typeof Header>> = (props) => <Header {...props} />;
+
+export const H2 = Template.bind({});
+H2.args = { themeClass: transitionTheme.variantP };
